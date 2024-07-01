f3598982ac249bce9d56b65232f608c2d9fc9e71
Larissa Oliveira <oliveira.larissa.dv@gmail.com>
2024-07-01 17:38:03 -0300

    chore: base project, simple entity with tests

---
+++ src/Lm.Streamthis.Catalog.Domain/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace Lm.Streamthis.Catalog.Domain.Entities;
+
+public class Category
+{
+    public Category(string name, string description)
+    {
+        Name = name;
+        Description = description;
+    }
+
+    public string Name { get; private set; }
+    public string Description { get; private set; }
+}
---
+++ tests/Lm.Streamthis.Catalog.UnitTests/Domain/Entities/Category/CategoryTests.cs
@@ -0,0 +1,23 @@
+﻿using DomainEntities = Lm.Streamthis.Catalog.Domain.Entities;
+
+namespace Lm.Streamthis.Catalog.UnitTests.Domain.Entities.Category;
+
+public class CategoryTests
+{
+    [Fact(DisplayName = nameof(Instantiate))]
+    [Trait("Domain", "Category")]
+    public void Instantiate()
+    {
+        var validData = new
+        {
+            Name = "category name",
+            Description = "category description"
+        };
+
+        var category = new DomainEntities.Category(validData.Name, validData.Description);
+
+        Assert.NotNull(category);
+        Assert.Equal(category.Name, validData.Name);
+        Assert.Equal(category.Description, validData.Description);
+    }
+}
