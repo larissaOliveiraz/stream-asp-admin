cbb5af3cb0ac38f9cb45798d66b903b78be1aede
Larissa Oliveira <oliveira.larissa.dv@gmail.com>
2024-07-11 17:22:54 -0300

    test: write tests for get category use case

---
+++ tests/Lm.Streamthis.Catalog.UnitTests/Application/GetCategory/GetCategoryFixture.cs
@@ -0,0 +1,42 @@
+﻿using Lm.Streamthis.Catalog.Domain.Entities;
+using Lm.Streamthis.Catalog.Domain.Repositories;
+using Lm.Streamthis.Catalog.UnitTests.Common;
+using Moq;
+
+namespace Lm.Streamthis.Catalog.UnitTests.Application.GetCategory;
+
+public class GetCategoryFixture : BaseFixture
+{
+    public Mock<ICategoryRepository> GetMockRepository() => new();
+
+    private string GetValidCategoryName()
+    {
+        var categoryName = "";
+        
+        while (categoryName.Length < 3)
+            categoryName = Faker.Commerce.Categories(1)[0];
+        
+        if (categoryName.Length > 255)
+            categoryName = categoryName[..255];
+        
+        return categoryName;
+    }
+
+    private string GetValidCategoryDescription()
+    {
+        var categoryDescription = Faker.Commerce.ProductDescription();
+        
+        if (categoryDescription.Length > 10_000)
+            categoryDescription = categoryDescription[..10_000];
+        
+        return categoryDescription;
+    }
+    
+    public Category GetValidCategory() =>
+        new(GetValidCategoryName(),GetValidCategoryDescription());
+}
+
+[CollectionDefinition(nameof(GetCategoryFixture))]
+public class GetCategoryFixtureCollection : ICollectionFixture<GetCategoryFixture>
+{
+}
---
+++ tests/Lm.Streamthis.Catalog.UnitTests/Application/GetCategory/GetCategoryTest.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Moq;
+using Lm.Streamthis.Catalog.Application.UseCases.Category.GetCategory;
+using UseCases = Lm.Streamthis.Catalog.Application.UseCases.Category.GetCategory;
+
+namespace Lm.Streamthis.Catalog.UnitTests.Application.GetCategory;
+
+[Collection(nameof(GetCategoryFixture))]
+public class GetCategoryTest(GetCategoryFixture fixture)
+{
+    [Fact(DisplayName = nameof(Should_Get_Category))]
+    [Trait("Application", "Get Category")]
+    public async void Should_Get_Category()
+    {
+        var repositoryMock = fixture.GetMockRepository();
+        var category = fixture.GetValidCategory();
+
+        repositoryMock
+            .Setup(x => 
+                x.Get(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(category);
+
+        var request = new GetCategoryRequest(category.Id);
+
+        var useCase = new UseCases.GetCategory(repositoryMock.Object);
+
+        var response = await useCase.Handle(request, CancellationToken.None);
+
+        repositoryMock.Verify(repository => 
+            repository.Get(It.IsAny<Guid>(), It.IsAny<CancellationToken>()), 
+            Times.Once);
+        response.Should().NotBeNull();
+        response.Name.Should().Be(category.Name);
+        response.Description.Should().Be(category.Description);
+        response.IsActive.Should().Be(category.IsActive);
+        response.Id.Should().Be(category.Id);
+        response.CreatedAt.Should().Be(category.CreatedAt);
+    }
+}
