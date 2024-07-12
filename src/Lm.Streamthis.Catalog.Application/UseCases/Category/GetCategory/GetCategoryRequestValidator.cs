8904db3c72d4b5e300dab3fb92dc1af9ba78c9c3
Larissa Oliveira <oliveira.larissa.dv@gmail.com>
2024-07-12 14:23:39 -0300

    feat: add request validation, tested

---
+++ src/Lm.Streamthis.Catalog.Application/UseCases/Category/GetCategory/GetCategoryRequestValidator.cs
@@ -0,0 +1,9 @@
+﻿using FluentValidation;
+
+namespace Lm.Streamthis.Catalog.Application.UseCases.Category.GetCategory;
+
+public class GetCategoryRequestValidator : AbstractValidator<GetCategoryRequest>
+{
+    public GetCategoryRequestValidator() =>
+        RuleFor(x => x.Id).NotEmpty();
+}
---
+++ tests/Lm.Streamthis.Catalog.UnitTests/Application/GetCategory/GetCategoryRequestValidatorTest.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using Lm.Streamthis.Catalog.Application.UseCases.Category.GetCategory;
+
+namespace Lm.Streamthis.Catalog.UnitTests.Application.GetCategory;
+
+[Collection(nameof(GetCategoryFixture))]
+public class GetCategoryRequestValidatorTest(GetCategoryFixture fixture)
+{
+    [Fact(DisplayName = nameof(Should_Validate_Request))]
+    [Trait("Application", "Get Category Request Validator")]
+    public void Should_Validate_Request()
+    {
+        var validId = Guid.NewGuid();
+        var validRequest = new GetCategoryRequest(validId);
+
+        var validator = new GetCategoryRequestValidator();
+
+        var result = validator.Validate(validRequest);
+
+        result.Should().NotBeNull();
+        result.IsValid.Should().BeTrue();
+        result.Errors.Should().HaveCount(0);
+    }
+
+    [Fact(DisplayName = nameof(Should_Throw_Exception_When_Request_IsInvalid))]
+    [Trait("Application", "Get Category Request Validator")]
+    public void Should_Throw_Exception_When_Request_IsInvalid()
+    {
+        var invalidId = Guid.Empty;
+        var invalidRequest = new GetCategoryRequest(invalidId);
+
+        var validator = new GetCategoryRequestValidator();
+
+        var result = validator.Validate(invalidRequest);
+
+        result.Should().NotBeNull();
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().HaveCountGreaterThan(0);
+    }
+}
