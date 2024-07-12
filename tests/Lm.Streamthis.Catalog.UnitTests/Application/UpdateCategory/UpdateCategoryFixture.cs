9fe3feb22a7ce7122f93eaf6b4e7f3551e10c62e
Larissa Oliveira <oliveira.larissa.dv@gmail.com>
2024-07-12 19:30:07 -0300

    test: write tests for update category use case

---
+++ tests/Lm.Streamthis.Catalog.UnitTests/Application/UpdateCategory/UpdateCategoryFixture.cs
@@ -0,0 +1,49 @@
+﻿using Lm.Streamthis.Catalog.Application.Interfaces;
+using Lm.Streamthis.Catalog.Domain.Entities;
+using Lm.Streamthis.Catalog.Domain.Repositories;
+using Lm.Streamthis.Catalog.UnitTests.Common;
+using Moq;
+
+namespace Lm.Streamthis.Catalog.UnitTests.Application.UpdateCategory;
+
+public class UpdateCategoryFixture : BaseFixture
+{
+    public string GetValidCategoryName()
+    {
+        var categoryName = "";
+        
+        while (categoryName.Length < 3)
+            categoryName = Faker.Commerce.Categories(1)[0];
+        
+        if (categoryName.Length > 255)
+            categoryName = categoryName[..255];
+        
+        return categoryName;
+    }
+
+    public string GetValidCategoryDescription()
+    {
+        var categoryDescription = Faker.Commerce.ProductDescription();
+        
+        if (categoryDescription.Length > 10_000)
+            categoryDescription = categoryDescription[..10_000];
+        
+        return categoryDescription;
+    }
+    
+    public static bool GetRandomBoolean() => new Random().NextDouble() < 0.5;
+    
+    public Category GetValidCategory() =>
+        new(GetValidCategoryName(),
+            GetValidCategoryDescription(), 
+            GetRandomBoolean());
+    
+    public Mock<ICategoryRepository> GetMockRepository() => new();
+    
+    public Mock<IUnitOfWork> GetMockUnitOfWork() => new();
+}
+
+[CollectionDefinition(nameof(UpdateCategoryFixture))]
+public class UpdateCategoryFixtureCollection : ICollectionFixture<UpdateCategoryFixture>
+{
+}
---
+++ tests/Lm.Streamthis.Catalog.UnitTests/Application/UpdateCategory/UpdateCategoryTest.cs
@@ -0,0 +1,49 @@
+﻿using Moq;
+using FluentAssertions;
+using Lm.Streamthis.Catalog.Application.UseCases.Category.UpdateCategory;
+using UseCase = Lm.Streamthis.Catalog.Application.UseCases.Category.UpdateCategory;
+
+namespace Lm.Streamthis.Catalog.UnitTests.Application.UpdateCategory;
+
+[Collection(nameof(UpdateCategoryFixture))]
+public class UpdateCategoryTest(UpdateCategoryFixture fixture)
+{
+    [Fact(DisplayName = nameof(Should_Update_Category))]
+    [Trait("Application", "Update Category")]
+    public async void Should_Update_Category()
+    {
+        var repositoryMock = fixture.GetMockRepository();
+        var unitOfWork = fixture.GetMockUnitOfWork();
+
+        var validCategory = fixture.GetValidCategory();
+
+        repositoryMock
+            .Setup(x =>
+                x.Get(validCategory.Id, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(validCategory);
+
+        var request = new UpdateCategoryRequest(
+            validCategory.Id,
+            fixture.GetValidCategoryName(),
+            fixture.GetValidCategoryDescription(),
+            !validCategory.IsActive
+        );
+
+        var useCase = new UseCase.UpdateCategory(repositoryMock.Object, unitOfWork.Object);
+        var response = await useCase.Handle(request, CancellationToken.None);
+
+        response.Should().NotBeNull();
+        response.Name.Should().Be(request.Name);
+        response.Description.Should().Be(request.Description);
+        response.IsActive.Should().Be(request.IsActive);
+        repositoryMock.Verify(repository => 
+            repository.Get(validCategory.Id, It.IsAny<CancellationToken>()),
+            Times.Once);
+        repositoryMock.Verify(repository => 
+            repository.Update(validCategory, It.IsAny<CancellationToken>()),
+            Times.Once);
+        unitOfWork.Verify(uow => 
+            uow.Commit(It.IsAny<CancellationToken>()),
+            Times.Once);
+    }
+}
