dcc06264fafed8bcdcb444c2ba5c9481a4270e12
Larissa Oliveira <oliveira.larissa.dv@gmail.com>
2024-07-16 17:15:16 -0300

    feat: create a request validator

---
+++ src/Lm.Streamthis.Catalog.Application/UseCases/Category/UpdateCategory/UpdateCategoryRequestValidator.cs
@@ -0,0 +1,9 @@
+﻿using FluentValidation;
+
+namespace Lm.Streamthis.Catalog.Application.UseCases.Category.UpdateCategory;
+
+public class UpdateCategoryRequestValidator : AbstractValidator<UpdateCategoryRequest>
+{
+    public UpdateCategoryRequestValidator() =>
+        RuleFor(x => x.Id).NotEmpty();
+}
