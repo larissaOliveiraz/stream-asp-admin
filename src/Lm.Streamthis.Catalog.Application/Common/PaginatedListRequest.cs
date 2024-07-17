c8cd70c3b29ef8a5de94e38a37af6808c574eff2
Larissa Oliveira <oliveira.larissa.dv@gmail.com>
2024-07-17 19:42:04 -0300

    feat: add common paginated list request and response for use cases

---
+++ src/Lm.Streamthis.Catalog.Application/Common/PaginatedListRequest.cs
@@ -0,0 +1,12 @@
+﻿using Lm.Streamthis.Catalog.Domain.SeedWork.SearchableRepository;
+
+namespace Lm.Streamthis.Catalog.Application.Common;
+
+public abstract class PaginatedListRequest(int page, int perPage, string search, string sort, SearchOrder order) 
+{
+    public int Page { get; set; } = page;
+    public int PerPage { get; set; } = perPage;
+    public string Search { get; set; } = search;
+    public string Sort { get; set; } = sort;
+    public SearchOrder Order { get; set; } = order;
+}
---
+++ src/Lm.Streamthis.Catalog.Application/Common/PaginatedListResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Lm.Streamthis.Catalog.Application.Common;
+
+public abstract class PaginatedListResponse<TResponse>(int page, int perPage, int total, List<TResponse> items)
+{
+    public int Page { get; set; } = page;
+    public int PerPage { get; set; } = perPage;
+    public int Total { get; set; } = total;
+    public List<TResponse> Items { get; set; } = items;
+}
