04709c28016ada12ddd5f718b094a04ece25e451
Larissa Oliveira <oliveira.larissa.dv@gmail.com>
2024-07-19 14:58:49 -0300

    test: write test when should list categories when list is empty

--- tests/Lm.Streamthis.Catalog.UnitTests/Application/ListCategories/ListCategoriesTest.cs
+++ tests/Lm.Streamthis.Catalog.UnitTests/Application/ListCategories/ListCategoriesTest.cs
@@ -28,65 +28,7 @@ public class ListCategoriesTest(ListCategoriesFixture fixture)
         repositoryMock
             .Setup(x =>
                 x.Search(
-                    It.Is<SearchRequest>(searchRequest => 
-                        searchRequest.Page == request.Page &&
-                        searchRequest.PerPage == request.PerPage &&
-                        searchRequest.Search == request.Search &&
-                        searchRequest.OrderBy == request.Sort &&
-                        searchRequest.Order == request.Order), 
-                    It.IsAny<CancellationToken>()))
-            .ReturnsAsync(searchResponse);
-
-        var useCase = new UseCase.ListCategories(repositoryMock.Object);
-        var response = await useCase.Handle(request, CancellationToken.None);
-
-        response.Should().NotBeNull();
-        response.Page.Should().Be(searchResponse.CurrentPage);
-        response.PerPage.Should().Be(searchResponse.PerPage);
-        response.Total.Should().Be(searchResponse.Total);
-        response.Items.Should().HaveCount(searchResponse.Items.Count);
-        response.Items.ForEach(item =>
-        {
-            var categorySearchResponse = searchResponse.Items.Find(category => category.Id == item.Id);
-            item.Should().NotBeNull();
-            item.Name.Should().Be(categorySearchResponse!.Name);
-            item.Description.Should().Be(categorySearchResponse.Description);
-            item.IsActive.Should().Be(categorySearchResponse.IsActive);
-            item.Id.Should().Be(categorySearchResponse.Id);
-            item.CreatedAt.Should().Be(categorySearchResponse.CreatedAt);
-        });
-        repositoryMock.Verify(repository =>
-            repository.Search(
-                It.Is<SearchRequest>(searchRequest =>
-                    searchRequest.Page == request.Page &&
-                    searchRequest.PerPage == request.PerPage &&
-                    searchRequest.Search == request.Search &&
-                    searchRequest.OrderBy == request.Sort &&
-                    searchRequest.Order == request.Order), 
-                It.IsAny<CancellationToken>()),
-            Times.Once);
-    }
-
-    [Theory(DisplayName = nameof(Should_List_Categories_Without_All_Request_Parameters))]
-    [Trait("Application", "List Categories")]
-    [MemberData(
-        nameof(ListCategoriesDataGenerator.GetValidRequestsWithoutAnyParameters),
-        parameters: 12,
-        MemberType = typeof(ListCategoriesDataGenerator))]
-    public async void Should_List_Categories_Without_All_Request_Parameters(ListCategoriesRequest request)
-    {
-        var repositoryMock = fixture.GetMockRepository();
-
-        var searchResponse = new SearchResponse<Category>(
-            request.Page,
-            request.PerPage,
-            fixture.GetValidCategoryList(),
-            new Random().Next(50, 200));
-
-        repositoryMock
-            .Setup(x =>
-                x.Search(
-                    It.Is<SearchRequest>(searchRequest => 
+                    It.Is<SearchRequest>(searchRequest =>
                         searchRequest.Page == request.Page &&
                         searchRequest.PerPage == request.PerPage &&
                         searchRequest.Search == request.Search &&
@@ -97,7 +39,7 @@ public class ListCategoriesTest(ListCategoriesFixture fixture)
 
         var useCase = new UseCase.ListCategories(repositoryMock.Object);
         var response = await useCase.Handle(request, CancellationToken.None);
-        
+
         response.Should().NotBeNull();
         response.Page.Should().Be(searchResponse.CurrentPage);
         response.PerPage.Should().Be(searchResponse.PerPage);
@@ -120,7 +62,111 @@ public class ListCategoriesTest(ListCategoriesFixture fixture)
                         searchRequest.PerPage == request.PerPage &&
                         searchRequest.Search == request.Search &&
                         searchRequest.OrderBy == request.Sort &&
-                        searchRequest.Order == request.Order), 
+                        searchRequest.Order == request.Order),
+                    It.IsAny<CancellationToken>()),
+            Times.Once);
+    }
+
+    [Theory(DisplayName = nameof(Should_List_Categories_Without_All_Request_Parameters))]
+    [Trait("Application", "List Categories")]
+    [MemberData(
+        nameof(ListCategoriesDataGenerator.GetValidRequestsWithoutAnyParameters),
+        parameters: 12,
+        MemberType = typeof(ListCategoriesDataGenerator))]
+    public async void Should_List_Categories_Without_All_Request_Parameters(ListCategoriesRequest request)
+    {
+        var repositoryMock = fixture.GetMockRepository();
+        var categoryList = fixture.GetValidCategoryList();
+
+        var searchResponse = new SearchResponse<Category>(
+            request.Page,
+            request.PerPage,
+            categoryList,
+            new Random().Next(50, 200));
+
+        repositoryMock
+            .Setup(x =>
+                x.Search(
+                    It.Is<SearchRequest>(searchRequest =>
+                        searchRequest.Page == request.Page &&
+                        searchRequest.PerPage == request.PerPage &&
+                        searchRequest.Search == request.Search &&
+                        searchRequest.OrderBy == request.Sort &&
+                        searchRequest.Order == request.Order),
+                    It.IsAny<CancellationToken>()))
+            .ReturnsAsync(searchResponse);
+
+        var useCase = new UseCase.ListCategories(repositoryMock.Object);
+        var response = await useCase.Handle(request, CancellationToken.None);
+
+        response.Should().NotBeNull();
+        response.Page.Should().Be(searchResponse.CurrentPage);
+        response.PerPage.Should().Be(searchResponse.PerPage);
+        response.Total.Should().Be(searchResponse.Total);
+        response.Items.Should().HaveCount(searchResponse.Items.Count);
+        response.Items.ForEach(item =>
+        {
+            var categorySearchResponse = searchResponse.Items.Find(category => category.Id == item.Id);
+            item.Should().NotBeNull();
+            item.Name.Should().Be(categorySearchResponse!.Name);
+            item.Description.Should().Be(categorySearchResponse.Description);
+            item.IsActive.Should().Be(categorySearchResponse.IsActive);
+            item.Id.Should().Be(categorySearchResponse.Id);
+            item.CreatedAt.Should().Be(categorySearchResponse.CreatedAt);
+        });
+        repositoryMock.Verify(repository =>
+                repository.Search(
+                    It.Is<SearchRequest>(searchRequest =>
+                        searchRequest.Page == request.Page &&
+                        searchRequest.PerPage == request.PerPage &&
+                        searchRequest.Search == request.Search &&
+                        searchRequest.OrderBy == request.Sort &&
+                        searchRequest.Order == request.Order),
+                    It.IsAny<CancellationToken>()),
+            Times.Once);
+    }
+
+    [Fact(DisplayName = nameof(Should_List_Categories_When_Empty))]
+    [Trait("Application", "List Categories")]
+    public async void Should_List_Categories_When_Empty()
+    {
+        var repositoryMock = fixture.GetMockRepository();
+        var request = fixture.GetValidRequest();
+
+        var searchResponse = new SearchResponse<Category>(
+            request.Page,
+            request.PerPage,
+            [],
+            0);
+
+        repositoryMock
+            .Setup(x =>
+                x.Search(
+                    It.Is<SearchRequest>(searchRequest =>
+                        searchRequest.Page == request.Page &&
+                        searchRequest.PerPage == request.PerPage &&
+                        searchRequest.Search == request.Search &&
+                        searchRequest.OrderBy == request.Sort &&
+                        searchRequest.Order == request.Order),
+                    It.IsAny<CancellationToken>()))
+            .ReturnsAsync(searchResponse);
+
+        var useCase = new UseCase.ListCategories(repositoryMock.Object);
+        var response = await useCase.Handle(request, CancellationToken.None);
+
+        response.Should().NotBeNull();
+        response.Page.Should().Be(searchResponse.CurrentPage);
+        response.PerPage.Should().Be(searchResponse.PerPage);
+        response.Total.Should().Be(0);
+        response.Items.Should().HaveCount(0);
+        repositoryMock.Verify(repository =>
+                repository.Search(
+                    It.Is<SearchRequest>(searchRequest =>
+                        searchRequest.Page == request.Page &&
+                        searchRequest.PerPage == request.PerPage &&
+                        searchRequest.Search == request.Search &&
+                        searchRequest.OrderBy == request.Sort &&
+                        searchRequest.Order == request.Order),
                     It.IsAny<CancellationToken>()),
             Times.Once);
     }
