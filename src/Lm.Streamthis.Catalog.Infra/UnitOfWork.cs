1f476f6997d0a0741851bbc39a6ec64ee69bee26
Larissa Oliveira <oliveira.larissa.dv@gmail.com>
2024-07-30 20:23:27 -0300

    feat: implement unit of work

---
+++ src/Lm.Streamthis.Catalog.Infra/UnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using Lm.Streamthis.Catalog.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lm.Streamthis.Catalog.Infra;
+
+public class UnitOfWork(StreamAspDbContext context) : IUnitOfWork
+{
+    public Task Commit(CancellationToken cancellationToken) =>
+        context.SaveChangesAsync(cancellationToken);
+}
