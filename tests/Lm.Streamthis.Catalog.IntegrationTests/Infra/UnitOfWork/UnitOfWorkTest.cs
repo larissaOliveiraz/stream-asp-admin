65f68114f146daa8f0c7d8334a050b23187d1ff6
Larissa Oliveira <oliveira.larissa.dv@gmail.com>
2024-07-30 20:24:22 -0300

    tests: write tests for unit of work implementation

--- tests/Lm.Streamthis.Catalog.IntegrationTests/Infra/UnitOfWork/UnitOfWorkTest.cs
+++ tests/Lm.Streamthis.Catalog.IntegrationTests/Infra/UnitOfWork/UnitOfWorkTest.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentAssertions;
+using InfraUOW = Lm.Streamthis.Catalog.Infra;
 
 namespace Lm.Streamthis.Catalog.IntegrationTests.Infra.UnitOfWork;
 
 [Collection(nameof(UnitOfWorkFixture))]
-public class UnitOfWorkTest
+public class UnitOfWorkTest(UnitOfWorkFixture fixture)
 {
+    [Fact(DisplayName = nameof(Should_Commit))]
+    [Trait("Infra", "Unit of Work")]
+    public async void Should_Commit()
+    {
+        var dbContext = fixture.CreateDbContext();
+        var categoriesList = fixture.GetValidCategoryList(20);
+
+        await dbContext.AddRangeAsync(categoriesList);
+
+        var unitOfWork = new InfraUOW.UnitOfWork(dbContext);
+        await unitOfWork.Commit(CancellationToken.None);
+
+        var newDbContext = fixture.CreateDbContext(true);
+        var savedCategories = newDbContext.Categories().ToList();
+
+        savedCategories.Should().NotBeEmpty();
+        savedCategories.Should().HaveCount(categoriesList.Count);
+    }
 }
