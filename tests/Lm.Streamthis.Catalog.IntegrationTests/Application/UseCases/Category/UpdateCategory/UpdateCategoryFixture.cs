854c54d331f2401561c0ddfe63369b4acd4c5e55
Larissa Oliveira <oliveira.larissa.dv@gmail.com>
2024-08-02 19:10:04 -0300

    tests: base structure for update caegory integration test

---
+++ tests/Lm.Streamthis.Catalog.IntegrationTests/Application/UseCases/Category/UpdateCategory/UpdateCategoryFixture.cs
@@ -0,0 +1,10 @@
+﻿using Lm.Streamthis.Catalog.IntegrationTests.Application.UseCases.Category.Common;
+
+namespace Lm.Streamthis.Catalog.IntegrationTests.Application.UseCases.Category.UpdateCategory;
+
+public class UpdateCategoryFixture : CategoryBaseFixture
+{
+}
+
+[CollectionDefinition(nameof(UpdateCategoryFixture))]
+public class UpdateCategoryFixtureCollection : ICollectionFixture<UpdateCategoryFixture> { }
---
+++ tests/Lm.Streamthis.Catalog.IntegrationTests/Application/UseCases/Category/UpdateCategory/UpdateCategoryTest.cs
@@ -0,0 +1,7 @@
+﻿namespace Lm.Streamthis.Catalog.IntegrationTests.Application.UseCases.Category.UpdateCategory;
+
+[Collection(nameof(UpdateCategoryFixture))]
+public class UpdateCategoryTest(UpdateCategoryFixture fixture)
+{
+
+}
