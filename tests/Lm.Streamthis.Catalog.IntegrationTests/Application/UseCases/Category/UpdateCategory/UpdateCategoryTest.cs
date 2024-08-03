131ca429a961e017cbe680055e5acc36d8e86172
Larissa Oliveira <oliveira.larissa.dv@gmail.com>
2024-08-03 18:47:19 -0300

    tests: integration test for update category use case without isactive

--- tests/Lm.Streamthis.Catalog.IntegrationTests/Application/UseCases/Category/UpdateCategory/UpdateCategoryTest.cs
+++ tests/Lm.Streamthis.Catalog.IntegrationTests/Application/UseCases/Category/UpdateCategory/UpdateCategoryTest.cs
@@ -46,4 +46,45 @@ public class UpdateCategoryTest(UpdateCategoryFixture fixture)
         updatedCategory.IsActive.Should().Be((bool)response.IsActive);
         updatedCategory.CreatedAt.Should().Be(response.CreatedAt);
     }
+
+    [Theory(DisplayName = nameof(Should_Update_Category_Without_IsActive))]
+    [Trait("Application", "Update Category")]
+    [MemberData(
+        nameof(UpdateCategoryDataGenerator.GetCategoriesToUpdate),
+        parameters: 5,
+        MemberType = typeof(UpdateCategoryDataGenerator))]
+    public async void Should_Update_Category_Without_IsActive(
+        DomainEntities.Category category, UpdateCategoryRequest request)
+    {
+        var dbContext = fixture.CreateDbContext();
+
+        await dbContext.AddRangeAsync(fixture.GetValidCategoryList(10));
+        var trackingInfo = await dbContext.AddAsync(category);
+        await dbContext.SaveChangesAsync();
+        trackingInfo.State = EntityState.Detached;
+
+        var repository = new CategoryRepository(dbContext);
+        var unitOfWork = new UnitOfWork(dbContext);
+
+        var requestWithoutIsActive = new UpdateCategoryRequest(
+            request.Id,
+            request.Name,
+            request.Description);
+
+        var useCase = new UseCase.UpdateCategory(repository, unitOfWork);
+        var response = await useCase.Handle(requestWithoutIsActive, CancellationToken.None);
+
+        response.Should().NotBeNull();
+        response.Id.Should().Be(request.Id);
+        response.Name.Should().Be(request.Name);
+        response.Description.Should().Be(request.Description);
+        response.IsActive.Should().Be(category.IsActive);
+
+        var updatedCategory = await fixture.CreateDbContext(true).Categories().FindAsync(request.Id);
+        updatedCategory.Should().NotBeNull();
+        updatedCategory.Name.Should().Be(request.Name);
+        updatedCategory.Description.Should().Be(request.Description);
+        updatedCategory.IsActive.Should().Be(category.IsActive);
+        updatedCategory.CreatedAt.Should().Be(category.CreatedAt);
+    }
 }
