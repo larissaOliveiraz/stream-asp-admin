deb810e480716ca967c651a579c3314fb41dd4c9
Larissa Oliveira <oliveira.larissa.dv@gmail.com>
2024-08-06 18:51:03 -0300

    tests: integration test for delete category use case throw exception when category not found

--- tests/Lm.Streamthis.Catalog.IntegrationTests/Application/UseCases/Category/DeleteCategory/DeleteCategoryTest.cs
+++ tests/Lm.Streamthis.Catalog.IntegrationTests/Application/UseCases/Category/DeleteCategory/DeleteCategoryTest.cs
@@ -1,4 +1,5 @@
 ﻿using FluentAssertions;
+using Lm.Streamthis.Catalog.Application.Exceptions;
 using Lm.Streamthis.Catalog.Application.UseCases.Category.DeleteCategory;
 using Lm.Streamthis.Catalog.Infra;
 using Lm.Streamthis.Catalog.Infra.Repositories;
@@ -33,6 +34,33 @@ public class DeleteCategoryTest(DeleteCategoryFixture fixture)
         var deletedCategory = await newDbContext.Categories().FindAsync(category.Id);
         deletedCategory.Should().BeNull();
         newDbContext.Categories().Should().HaveCount(0);
+    }
 
+    [Fact(DisplayName = nameof(Should_Throw_Exception_When_Category_NotFound))]
+    [Trait("Integration - Application", "Delete Category")]
+    public async void Should_Throw_Exception_When_Category_NotFound()
+    {
+        var dbContext = fixture.CreateDbContext();
+        var category = fixture.GetCategory();
+
+        var trackingInfo = await dbContext.AddAsync(category);
+        await dbContext.SaveChangesAsync();
+        trackingInfo.State = EntityState.Detached;
+
+        var repository = new CategoryRepository(dbContext);
+        var unitOfWork = new UnitOfWork(dbContext);
+
+        var request = new DeleteCategoryRequest(Guid.NewGuid());
+
+        var useCase = new UseCase.DeleteCategory(repository, unitOfWork);
+        var action = async () =>
+            await useCase.Handle(request, CancellationToken.None);
+
+        await action.Should()
+            .ThrowAsync<NotFoundException>()
+            .WithMessage($"Category with id '{request.Id}' was not found.");
+
+        var newDbContext = fixture.CreateDbContext(true);
+        newDbContext.Categories().Should().HaveCount(1);
     }
 }
