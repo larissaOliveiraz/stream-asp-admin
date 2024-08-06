be46568be010def4f434691d13e4b468214d1fc2
Larissa Oliveira <oliveira.larissa.dv@gmail.com>
2024-08-06 17:47:20 -0300

    tests: delete category integration tests base structure

---
+++ tests/Lm.Streamthis.Catalog.IntegrationTests/Application/UseCases/Category/DeleteCategory/DeleteCategoryFixture.cs
@@ -0,0 +1,10 @@
+﻿using Lm.Streamthis.Catalog.IntegrationTests.Application.UseCases.Category.Common;
+
+namespace Lm.Streamthis.Catalog.IntegrationTests.Application.UseCases.Category.DeleteCategory;
+
+public class DeleteCategoryFixture : CategoryBaseFixture
+{
+}
+
+[CollectionDefinition(nameof(DeleteCategoryFixture))]
+public class DeleteCategoryFixtureCollection : ICollectionFixture<DeleteCategoryFixture> { }
---
+++ tests/Lm.Streamthis.Catalog.IntegrationTests/Application/UseCases/Category/DeleteCategory/DeleteCategoryTest.cs
@@ -0,0 +1,6 @@
+﻿namespace Lm.Streamthis.Catalog.IntegrationTests.Application.UseCases.Category.DeleteCategory;
+
+[Collection(nameof(DeleteCategoryFixture))]
+public class DeleteCategoryTest(DeleteCategoryFixture fixture)
+{
+}
