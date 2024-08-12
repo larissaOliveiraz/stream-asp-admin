82649bd5e5413a5aa87bb8206e11c7593ea1c267
Larissa Oliveira <oliveira.larissa.dv@gmail.com>
2024-08-12 17:06:12 -0300

    tests: integration test for list categories use case search by text

--- tests/Lm.Streamthis.Catalog.IntegrationTests/Application/UseCases/Category/ListCategories/ListCategoriesFixture.cs
+++ tests/Lm.Streamthis.Catalog.IntegrationTests/Application/UseCases/Category/ListCategories/ListCategoriesFixture.cs
@@ -1,9 +1,17 @@
-﻿using Lm.Streamthis.Catalog.IntegrationTests.Application.UseCases.Category.Common;
+﻿using DomainEntities = Lm.Streamthis.Catalog.Domain.Entities;
+using Lm.Streamthis.Catalog.IntegrationTests.Application.UseCases.Category.Common;
 
 namespace Lm.Streamthis.Catalog.IntegrationTests.Application.UseCases.Category.ListCategories;
 
 public class ListCategoriesFixture : CategoryBaseFixture
 {
+    public List<DomainEntities.Category> GetCategoriesListWithNames(List<string> names) =>
+        names.Select(name =>
+        {
+            var category = GetCategory();
+            category.Update(name);
+            return category;
+        }).ToList();
 }
 
 [CollectionDefinition(nameof(ListCategoriesFixture))]
--- tests/Lm.Streamthis.Catalog.IntegrationTests/Application/UseCases/Category/ListCategories/ListCategoriesTest.cs
+++ tests/Lm.Streamthis.Catalog.IntegrationTests/Application/UseCases/Category/ListCategories/ListCategoriesTest.cs
@@ -96,4 +96,43 @@ public class ListCategoriesTest(ListCategoriesFixture fixture)
             item.CreatedAt.Should().Be(category.CreatedAt);
         });
     }
+
+    [Theory(DisplayName = nameof(Should_Search_By_Text))]
+    [Trait("Integration - Application", "List Categories")]
+    [InlineData("Romance", 1, 5, 2, 2)]
+    [InlineData("Fantasy", 1, 2, 2, 3)]
+    [InlineData("Fantasy", 2, 5, 0, 3)]
+    [InlineData("Fantasy", 2, 2, 1, 3)]
+    [InlineData("Random", 1, 5, 0, 0)]
+    public async void Should_Search_By_Text(
+        string search, int currentPage, int perPage, int expectedItemsInCurrentPage, int expectedTotalItems)
+    {
+        var dbContext = fixture.CreateDbContext();
+        var categories = fixture.GetCategoriesListWithNames([
+            "Fantasy", "Romance", "Sci-Fi", "Horror", "Drama", "Fantasy Romance", "Comedy", "Historical Fantasy"]);
+
+        await dbContext.AddRangeAsync(categories);
+        await dbContext.SaveChangesAsync();
+
+        var repository = new CategoryRepository(dbContext);
+        var request = new ListCategoriesRequest(currentPage, perPage, search);
+
+        var useCase = new UseCase.ListCategories(repository);
+        var response = await useCase.Handle(request, CancellationToken.None);
+
+        response.Should().NotBeNull();
+        response.Items.Should().HaveCount(expectedItemsInCurrentPage);
+        response.Total.Should().Be(expectedTotalItems);
+        response.Page.Should().Be(currentPage);
+        response.PerPage.Should().Be(perPage);
+        response.Items.ForEach(item =>
+        {
+            var category = categories.Find(x => x.Id == item.Id);
+            item.Should().NotBeNull();
+            item.Name.Should().Be(category!.Name);
+            item.Description.Should().Be(category.Description);
+            item.IsActive.Should().Be(category.IsActive);
+            item.CreatedAt.Should().Be(category.CreatedAt);
+        });
+    }
 }
